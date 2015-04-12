9d6a2d0548df7879fe0c9f7ebb7169e6b53e852c
Zhang Haoran <zhr19960917@vip.qq.com>
2015-04-12 22:58:28 +0800

    Add SMSUser

---
+++ src/com/bistuSMS/SMSUser.java
@@ -0,0 +1,15 @@
+package com.bistuSMS;
+
+/**
+ * Created by ZHRMoe on 15/4/12.
+ */
+public class SMSUser {
+
+    private String userName;
+    private String userPsw;
+
+    public SMSUser() {
+        this.userName = "test";
+        this.userPsw = "test";
+    }
+}
