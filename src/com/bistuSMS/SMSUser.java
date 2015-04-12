29ba5f0084b72d52e1cbb427f47185befd520bb7
Zhang Haoran <zhr19960917@vip.qq.com>
2015-04-13 01:21:13 +0800

    Update SMSUser
    Add a constructor method and 2 methods to manage it.

--- src/com/bistuSMS/SMSUser.java
+++ src/com/bistuSMS/SMSUser.java
@@ -3,6 +3,15 @@ package com.bistuSMS;
 /**
  * Created by ZHRMoe on 15/4/12.
  */
+
+/**
+ * SMSUser is a class to describe a single user to SMS.
+ * There are 2 properties in this class.
+ * This class provides 2 construction methods.
+ * This class provides 2 get/set methods.
+ * ZHRMoe Studio, 2015.
+ */
+
 public class SMSUser {
 
     private String userName;
@@ -12,4 +21,19 @@ public class SMSUser {
         this.userName = "test";
         this.userPsw = "test";
     }
+
+    public SMSUser(String userName, String userPsw) {
+        this.userName = userName;
+        this.userPsw = userPsw;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public void setUserPsw(String psw) {
+        this.userPsw = psw;
+    }
 }
+
+
