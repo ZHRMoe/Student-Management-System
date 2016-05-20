8831158097de54abda09baf1761e2a5a7522246e
billhu1996 <billhu1996@gmail.com>
2016-05-20 11:11:25 +0800

    Until now SMSXML can add/remove user and student.

--- src/com/bistuSMS/SMSXML.java
+++ src/com/bistuSMS/SMSXML.java
@@ -1,5 +1,6 @@
 package com.bistuSMS;
 
+import com.sun.xml.internal.bind.v2.TODO;
 import org.w3c.dom.*;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -13,6 +14,7 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
+import java.util.ArrayList;
 
 /**
  * Created by bill on 16/5/19.
@@ -31,32 +33,115 @@ public class SMSXML {
         transformer.transform(source, result);
     }
 
-    public static Document open(String fileName) throws Exception {
+    public static Document open() throws Exception {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         doc = builder.parse(fileName);
         return doc;
     }
 
-    public static void addUser() {
-
+    public static void addUser(SMSUser user) throws Exception {
+        try {
+            open();
+            NodeList userInfo = doc.getElementsByTagName("user");
+            int i = 0;
+            for (;i<userInfo.getLength();i++) {
+                Element userElement = (Element)userInfo.item(i);
+                if (userElement.getAttributeNode("userName").getTextContent().equals(user.getUserName())) {
+                    throw new Exception("用户名已存在!");
+                }
+            }
+            Node users = doc.getElementsByTagName("userInfo").item(0);
+            Element userElement = doc.createElement("user");
+            userElement.setAttribute("userName", user.getUserName());
+            userElement.setAttribute("userPsw", user.getUserPsw());
+            users.appendChild(userElement);
+            save();
+        } catch (Exception e) {
+            if (e.getMessage().equals("用户名已存在!")) {
+                throw e;
+            }
+            e.printStackTrace();
+        }
     }
 
-    public static void removeUser() {
-
+    public static void removeUser(SMSUser user) {
+        try {
+            open();
+            NodeList userInfo = doc.getElementsByTagName("user");
+            for (int i = 0;i<userInfo.getLength();i++) {
+                Element userElement = (Element) userInfo.item(i);
+                if (userElement.getAttributeNode("userName").getTextContent().equals(user.getUserName()) && userElement.getAttributeNode("userPsw").getTextContent().equals(user.getUserPsw())) {
+                    Node userInfoNode = doc.getElementsByTagName("userInfo").item(0);
+                    userInfoNode.removeChild(userElement);
+                    break;
+                }
+            }
+            save();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void addStudent() {
-
+    public static void addStudent(SMSStudent student) throws Exception {
+        try {
+            open();
+            NodeList studentInfo = doc.getElementsByTagName("student");
+            int i = 0;
+            for (;i<studentInfo.getLength();i++) {
+                Element studentElement = (Element) studentInfo.item(i);
+                if (studentElement.getAttributeNode("stuNumber").getTextContent().equals(student.getStuNumber())) {
+                    throw new Exception("学号重复!");
+                }
+            }
+            if (i == studentInfo.getLength()) {
+                Node students = doc.getElementsByTagName("studentInfo").item(0);
+                Element studentElement = doc.createElement("student");
+                Element stuName = doc.createElement("stuName");
+                stuName.setNodeValue(student.getStuName());
+                Element stuDom = doc.createElement("stuDom");
+                stuDom.setNodeValue(student.getStuDorm());
+                Element joinCCP = doc.createElement("joinCCP");
+                joinCCP.setNodeValue(student.getJoinCCP());
+                studentElement.setAttribute("stuClass",student.getStuClass());
+                studentElement.setAttribute("stuNumber",student.getStuNumber());
+                studentElement.setAttribute("stuSex",student.getStuSex());
+                studentElement.appendChild(stuName);
+                studentElement.appendChild(stuDom);
+                studentElement.appendChild(joinCCP);
+                students.appendChild(studentElement);
+                save();
+            }
+        } catch (Exception e) {
+            if (e.getMessage().equals("学号重复!")) {
+                throw e;
+            }
+            e.printStackTrace();
+        }
     }
 
-    public static void removeStudent() {
-
+    public static void removeStudent(SMSStudent student) {
+        try {
+            open();
+            NodeList studentInfo = doc.getElementsByTagName("student");
+            Element studentElement = null;
+            for (int i = 0;i<studentInfo.getLength();i++) {
+                studentElement = (Element) studentInfo.item(i);
+                if (studentElement.getAttributeNode("stuNumber").getTextContent().equals(student.getStuNumber())) {
+                    Node students = doc.getElementsByTagName("studentInfo").item(0);
+                    students.removeChild(studentElement);
+                    save();
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static SMSUserList getUsers() {
         try {
-            open(fileName);
+            open();
             NodeList userInfo = doc.getElementsByTagName("user");
             SMSUserList list = new SMSUserList();
             for (int i = 0;i<userInfo.getLength();i++) {
@@ -75,7 +160,7 @@ public class SMSXML {
 
     public static SMSStudentArray getStudents() {
         try {
-            open(fileName);
+            open();
             NodeList studentInfo = doc.getElementsByTagName("student");
             SMSStudentArray studentArray = new SMSStudentArray();
             for (int i = 0;i<studentInfo.getLength();i++) {
@@ -98,9 +183,12 @@ public class SMSXML {
 
     public static void main(String[] args) {
         try {
-            SMSXML.getStudents();
-            SMSXML.getUsers();
-            SMSXML.save(doc);
+            getStudents();
+            getUsers();
+//            removeUser(new SMSUser("abcd", "5f4dcc3b5aa765d61d8327deb882cf99"));
+            addStudent(new SMSStudent("","","","201","",""));
+            addUser(new SMSUser("abc","password"));
+            save();
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
             Document doc = builder.parse("SystemSupports.xml");
@@ -139,7 +227,7 @@ public class SMSXML {
 //                email.appendChild(doc.createTextNode("ls@tom.com"));
 //                student.appendChild(email);
 //            }
-            save(doc);
+            save();
         } catch (Exception e) {
             e.printStackTrace();
         }
